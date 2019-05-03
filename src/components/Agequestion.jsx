80b253a36eb3c5f6def0a66f9615029f95be0bb6
lm <leo.mangutov@gmail.com>
2019-05-03 10:35:57 -0700

    Add Age question page to confirm age is over 21

---
+++ src/components/Agequestion.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function Agequestion(props){
+  return (
+    <div>
+      <h1>Welcome to the Drink App</h1>
+      <p>Please confirm you are over the age of 21</p>
+      <button onClick={}>Yes, I am over 21</button>
+      <button onClick={}>No, but I would like to see the non alcoholic drinks</button>
+    </div>
+  );
+}
+
+// Agequestion.propTypes = {
+//   onTroubleshootingConfirmation: PropTypes.func
+// };
+
+export default Agequestion;
