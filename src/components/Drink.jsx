059fd42ff2c79907ea7e750471f3c16062388093
lm <leo.mangutov@gmail.com>
2019-05-03 11:30:41 -0700

    add drink component

---
+++ src/components/Drink.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Drink(props){
+  const drinkInformation =
+    <div>
+      <h2>{props.name}</h2>
+      <h4>{props.ingredients}</h4>
+      <hr/>
+    </div>;
+}
+
+Drink.propTypes = {
+  name: PropTypes.string.isRequired,
+  ingredients: PropTypes.string.isRequired
+  // issue: PropTypes.string,
+  // formattedWaitTime: PropTypes.string.isRequired,
+  // currentRouterPath: PropTypes.string,
+  // onDrinkSelection: PropTypes.func,
+  // ticketId: PropTypes.string.isRequired
+};
+
+export default Drink;
