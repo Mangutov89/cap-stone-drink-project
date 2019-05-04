d2814d6099c68223b196e6756c4ea092d8d67a7d
lm <leo.mangutov@gmail.com>
2019-05-03 17:00:43 -0700

    added actual about section instead of lorem ipsum

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,6 +21,8 @@ function About(){
       `}
       </style>
       <h1>About Drink App</h1>
+      <p>Welcome to drink app, where you can search for any alcoholic and non alcoholic beverages. We were created when people couldn't find a beverage recipe website for both options, so we made one! For those people who want to become bartenders we also offer information where you can go an take your OLCC test to serve alcohol. If you cant find a drink on our website or if you wanted to create and store your own recipe, you can create an account thru google and you can store your recipes under your username, and you can favorite your favorite recipes that you found.
+       </p>
       <p>lore ipsum Lorem ipsum dolor sit amet, ex sea putent habemus pertinax, id agam quidam persequeris mea. Affert quando sit eu, ei nec nostrud persecuti pertinacia. Vitae soleat mnesarchum est ut, te qui enim legere platonem. Duo id habemus constituto instructior, nam et laudem ancillae detraxit. Cibo aliquid eligendi duo ut. Pri in justo volutpat corrumpit, duo cu sale omnis, nam cu malorum reformidans conclusionemque.
       At quo nemore detraxit, tamquam contentiones conclusionemque ut nec. Ex sed legere equidem utroque, has cu sale partem, mel posse quando ut. Vel summo fuisset ei, ne est vidisse nonumes. Dolores voluptaria reprehendunt eu mel, nonumes appareat iudicabit qui an.</p>
     </div>
