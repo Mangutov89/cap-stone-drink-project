6cc9eca267f399d88df61f7329825bb2399760e4
lm <leo.mangutov@gmail.com>
2019-05-10 15:23:38 -0700

    had trouble with the loop, but got it working now

--- src/components/DrinkList.jsx
+++ src/components/DrinkList.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
 import Drink from './Drink';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 
 
 function DrinkList(props) {
   return (
     <div>
-      <Drink/>
-      <hr/>
-      // {Object.keys(props.ticketList).map(function(ticketId) {
-      //   let ticket = props.ticketList[ticketId];
-      //   return <Drink names={ticket.names}
-      //     location={ticket.location}
-      //     issue={ticket.issue}
-      //     formattedWaitTime={ticket.formattedWaitTime}
-      //     currentRouterPath={props.currentRouterPath}
-      //     key={ticketId}
-      //     ticketId={ticketId}
-      //     onDrinkSelection={props.onDrinkSelection}/>
-      })}
+      {props.drinkList.map((drink, index) =>
+        <Drink name={drink.name}
+          ingredients={drink.ingredients}
+          id={drink.id}
+          key={index} />
+      )}
     </div>
   );
 }
 
 DrinkList.propTypes = {
-  ticketList: PropTypes.object,
-  currentRouterPath: PropTypes.string,
-  onDrinkSelection: PropTypes.func
+  drinkList: PropTypes.array
 };
 
 export default DrinkList;
