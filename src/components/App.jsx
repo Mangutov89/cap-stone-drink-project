159a42482b630a09a9ed124c0ff5b9cde522095c
lm <leo.mangutov@gmail.com>
2019-05-10 08:45:23 -0700

    add routes to different pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
 
-// import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
     <div>
       <Navbar/>
-
+      <Switch>
+        <Route exact path='/about' component= {About} />
+        <Route exact path='/results' component= {Results} />
+        <Route exact path='/create' component= {CreateNewDrinkForm} />
+        <Route exact path='/drinklist' component= {DrinkList} />
+      </Switch>
     </div>
   );
 }
