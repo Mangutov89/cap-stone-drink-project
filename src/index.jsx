73d2fa5022d13a3a64d6ea37c0b02cb4e9a9055e
lm <leo.mangutov@gmail.com>
2019-05-10 17:09:29 -0700

    trying to add store to save drinks locally

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './reducers/index';
+import logger from 'redux-logger';
 
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const render = (Component) => {
   ReactDOM.render(
